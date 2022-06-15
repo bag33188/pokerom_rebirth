e89a10a576a5d9b39d77bbbe10dd406d48d8f597
bag33188 <bglatman@outlook.com>
2022-06-14 21:12:22 -0700

    add tables

---
+++ database/migrations/2022_06_02_000001_create_files_collection.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Jenssegers\Mongodb\Schema\Blueprint;
+
+# php artisan migrate:refresh --path=database/migrations/2022_05_31_000001_create_files_collection.php
+# !!! php artisan migrate:rollback
+
+return new class extends Migration {
+
+    /**
+     * The name of the database connection to use.
+     *
+     * @var string
+     */
+    protected $connection = 'mongodb';
+
+    /**
+     * Execute migration within transaction IF AVAILABLE
+     *
+     * @var bool
+     */
+    public $withinTransaction = true;
+
+    // don't allow migrations
+    private const ALLOW_MIGRATIONS = false;
+
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        //!! never execute. current data is intended to be permanent !!//
+        if (self::ALLOW_MIGRATIONS === true) {
+            Schema::connection($this->connection)->create('roms.files', function (Blueprint $collection) {
+                $collection->index(['length', 'filename'], 'length_1_filename_1',
+                    options: ['unique' => true, 'partialFilterExpression' => ['filename' => ['$exists' => true]]]);
+            });
+            Schema::connection($this->connection)->create('roms.chunks', function (Blueprint $collection) {
+                $collection->index(['files_id', 'n'], 'files_id_1_n_1', options: ['unique' => true]);
+            });
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        /*! don't use down methods as it could overwrite current files in db */
+        if (self::ALLOW_MIGRATIONS === true) {
+            Schema::connection($this->connection)
+                ->table('roms.files', function (Blueprint $collection) {
+                    $collection->drop();
+                });
+            Schema::connection($this->connection)
+                ->table('roms.chunks', function (Blueprint $collection) {
+                    $collection->drop();
+                });
+        }
+    }
+};
---
+++ database/migrations/2022_06_15_040939_create_games_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $db_slug_comment = 'only the slug is a unique key. since the game name can be remotely similar through novelty character encodings.';
+        Schema::create('games', function (Blueprint $table) use ($db_slug_comment) {
+            $table->id()->autoIncrement();
+            $table->foreignId('rom_id')->unique()
+                ->references('id')->on('roms')
+                ->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('game_name', 40);
+            $table->enum('game_type', GAME_TYPES);
+            $table->date('date_released');
+            $table->tinyInteger('generation')->unsigned();
+            $table->enum('region', REGIONS);
+            $table->string('slug', 40 + 2)->nullable()->unique()->comment($db_slug_comment);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
