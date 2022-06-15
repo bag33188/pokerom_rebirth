46b50d290b00434631e3e2213496dc05d19fdbb6
bag33188 <bglatman@outlook.com>
2022-06-15 02:08:49 -0700

    fix

--- config/app.php
+++ config/app.php
@@ -17,6 +17,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'version' => env('APP_VERSION'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -41,7 +43,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
