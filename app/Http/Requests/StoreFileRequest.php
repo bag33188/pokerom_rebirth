3561c87ef7ed50a21faa9a278aaafdde4795226f
bag33188 <bglatman@outlook.com>
2022-06-15 02:51:38 -0700

    fix

--- app/Http/Requests/StoreFileRequest.php
+++ app/Http/Requests/StoreFileRequest.php
@@ -1,19 +1,28 @@
 <?php
-
 namespace App\Http\Requests;
 
+use App\Models\File;
+use App\Rules\ValidFilename;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreFileRequest extends FormRequest
 {
+    protected $stopOnFirstFailure = true;
+
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('create', File::class);
+    }
+
+    private function getFileNameIfExists(): string
+    {
+        $file = $this[FILE_FORM_KEY] ?? null;
+        return isset($file) ? $file->getClientOriginalName() : "ERR_NO_FILENAME";
     }
 
     /**
@@ -21,10 +30,13 @@ class StoreFileRequest extends FormRequest
      *
      * @return array<string, mixed>
      */
-    public function rules()
+    public function rules(): array
     {
-        return [
-            //
-        ];
+        $fileRules = [];
+        $fileRules[FILE_FORM_KEY] = array(
+            'required',
+            new ValidFilename($this->getFileNameIfExists()),
+        );
+        return $fileRules;
     }
 }
