de690e2bc11ce1ff32b9b39c0d4cc2575f56e829
bag33188 <bglatman@outlook.com>
2022-06-14 21:15:52 -0700

    add observer and notification

---
+++ app/Notifications/WelcomeNotification.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeNotification extends Notification
+{
+    use Queueable;
+
+    private string $username;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(string $username = null)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via(mixed $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return MailMessage
+     */
+    public function toMail(mixed $notifiable): MailMessage
+    {
+        $lnMsg = (isset($this->username) ? "$this->username, welcome" : 'Welcome') . ' to the world of Pok' . _EACUTE . 'ROM!';
+        return (new MailMessage)
+            ->subject('Thank you for joining ' . config('app.name') . '!!')
+            ->from(config('mail.from.address'))
+            ->line($lnMsg)
+            ->action('Check it out!', route('roms.index'))
+            ->line('enjoy!');
+    }
+}
