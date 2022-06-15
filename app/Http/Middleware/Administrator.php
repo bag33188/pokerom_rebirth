eb57aedf5603bcb705047fa8107fd2331049afc3
bag33188 <bglatman@outlook.com>
2022-06-14 21:07:05 -0700

    add admin middleware

---
+++ app/Http/Middleware/Administrator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class Administrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        if ($request->user()->isAdmin()) {
+            return $next($request);
+        } else {
+            if (!$request->expectsJson()) {
+                return response()->redirectTo(RouteServiceProvider::HOME);
+            }
+            throw new AccessDeniedHttpException("This action is unauthorized.");
+        }
+    }
+}
