6f8448e4b960076f22bca55dc9f6e778daad2bec
bag33188 <bglatman@outlook.com>
2022-06-16 05:04:22 -0700

    fix

--- app/Interfaces/UserServiceInterface.php
+++ app/Interfaces/UserServiceInterface.php
@@ -4,9 +4,13 @@ namespace App\Interfaces;
 
 use App\Models\User;
 
-interface UserServiceInterface {
+interface UserServiceInterface
+{
     public function authenticateUserAgainstCredentials(User $user, string $requestPassword);
+
     public function logoutCurrentUser();
+
     public function registerUserToken(User $user);
+
     public function deleteUserAndTokens(User $user);
 }
