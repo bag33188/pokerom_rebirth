710075b74566c3c5ba28b9fd153a8077fbb1868c
Broccolini <bglatman@callutheran.edu>
2022-06-15 23:17:51 -0700

    fix

--- utils/constants/entities.php
+++ utils/constants/entities.php
@@ -2,6 +2,7 @@
 
 /** the field name for file uploading */
 const FILE_FORM_KEY = 'file';
+/** the key name for sanctum personal access token */
 const API_TOKEN_KEY = 'auth_token';
 /** the special _**`e`**_ character in the Pokemon logo */
 const _EACUTE = "\u{00E9}";
