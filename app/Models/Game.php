ed97381f6ad21eb5adbabd7def9fdce5086fd192
bag33188 <bglatman@outlook.com>
2022-06-18 09:57:51 -0700

    fix

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -63,7 +63,7 @@ class Game extends Model
 
     public function setGameNameAttribute(string $value): void
     {
-        $this->attributes['game_name'] = preg_replace("/pokemon/i", 'Pokemon', $value);
+        $this->attributes['game_name'] = preg_replace("/^pokemon/i", 'Pokemon', $value);
     }
 
     public function setGameTypeAttribute(string $value): void
