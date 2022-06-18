afb81eb288a373d4641d298b3ec3dbefd5184c20
bag33188 <bglatman@outlook.com>
2022-06-18 13:06:27 -0700

    fix

--- utils/constants.php
+++ utils/constants.php
@@ -7,6 +7,7 @@ $constants_path = "constants";
 require_once "$constants_path/enums.php";
 require_once "$constants_path/patterns.php";
 require_once "$constants_path/entities.php";
-require_once "$constants_path/values.php";
+require_once "$constants_path/sizes.php";
+require_once "$constants_path/lengths.php";
 
 //--- END GLOBAL CONSTANTS ---//
---
+++ utils/constants/sizes.php
@@ -0,0 +1,11 @@
+<?php
+
+/** Unit: _`KB/1024`_, value: **`17` Gibibytes** */
+const MAX_ROM_SIZE = 17825792;
+const MIN_ROM_SIZE = 1020;
+
+/** Unit: _`Bytes`_, value: **`17` Gibibytes** */
+const MAX_FILE_SIZE = 18253611008;
+
+const MAX_GAME_GENERATION = 9;
+const MIN_GAME_GENERATION = 0;
