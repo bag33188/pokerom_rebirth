667569a6ecc63839922f4c80441fe67cd6e41720
bag33188 <bglatman@outlook.com>
2022-06-18 08:25:12 -0700

    fix

--- app/Events/FileUploaded.php
+++ app/Events/FileUploaded.php
@@ -5,16 +5,16 @@ namespace App\Events;
 use App\Models\File;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class FileUploaded
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public File $file;
+
     /**
      * Create a new event instance.
      *
@@ -23,5 +23,15 @@ class FileUploaded
     public function __construct(File $file)
     {
         $this->file = $file;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|PrivateChannel|array
+     */
+    public function broadcastOn(): Channel|PrivateChannel|array
+    {
+        return new PrivateChannel('channel-name');
     }
 }
