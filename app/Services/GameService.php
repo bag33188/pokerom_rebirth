fbe57884a051710aa011dd8cdb4a9a89e09b4d79
bag33188 <bglatman@outlook.com>
2022-06-18 14:52:45 -0700

    fix

--- app/Services/GameService.php
+++ app/Services/GameService.php
@@ -6,18 +6,19 @@ use App\Interfaces\GameServiceInterface;
 use App\Models\Game;
 use App\Models\Rom;
 
-class GameService implements GameServiceInterface {
+class GameService implements GameServiceInterface
+{
     public function createGame(int $romId, array $data): Game
     {
         $rom = Rom::findOrFail($romId);
         $game = $rom->game()->create($data);
-        return $this->associateGameWithRom($game, $romId);
+        $this->associateGameWithRom($game, $rom);
+        return $game;
     }
-    private function associateGameWithRom(Game $game, int $romId): Game
+
+    private function associateGameWithRom(Game $game, Rom $rom)
     {
-        $rom = Rom::find($romId);
         $game->rom()->associate($rom);
         $game->saveQuietly();
-        return $game;
     }
 }
