3145db3bf023a6efaf16941a5fc59f2fd5a63345
bag33188 <bglatman@outlook.com>
2022-06-18 17:20:14 -0700

    fix

--- app/Services/GameService.php
+++ app/Services/GameService.php
@@ -3,11 +3,19 @@
 namespace App\Services;
 
 use App\Interfaces\GameServiceInterface;
+use App\Interfaces\RomRepositoryInterface;
 use App\Models\Game;
 use App\Models\Rom;
 
-class GameService extends RomService implements GameServiceInterface
+class GameService implements GameServiceInterface
 {
+    protected RomRepositoryInterface $romRepository;
+
+    public function __construct(RomRepositoryInterface $romRepository)
+    {
+        $this->romRepository = $romRepository;
+    }
+
     public function createGame(int $romId, array $data): Game
     {
         $rom = $this->romRepository->findRomIfExists($romId);
