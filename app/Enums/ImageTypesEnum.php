b676e2392c6b12fbeb645791d07a6f9bc03c9139
bag33188 <bglatman@outlook.com>
2022-06-19 20:40:18 -0700

    fix

--- app/Enums/ImageTypesEnum.php
+++ app/Enums/ImageTypesEnum.php
@@ -5,6 +5,14 @@ namespace App\Enums;
 
 enum ImageTypesEnum: string
 {
+    // PORTABLE NETWORK GRAPHICS
     case IMG_PNG = "image/png";
+    // JOINT PHOTOGRAPHIC EXPERTS GROUP
     case IMG_JPG = "image/jpeg";
+    // TAGGED IMAGE FORMAT
+    case IMG_TFF = "image/tiff";
+    // GRAPHICS INTERCHANGE FORMAT
+    case IMG_GIF = "image/gif";
+    // ICON FORMAT
+    case IMG_ICO = "image/vnd.microsoft.icon";
 }
