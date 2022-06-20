fe2fd6b4e5f8aba8560475842f8ca7e82cc62ef9
bag33188 <bglatman@outlook.com>
2022-06-19 19:59:02 -0700

    fix

--- resources/views/ui/show-hide.blade.php
+++ resources/views/ui/show-hide.blade.php
@@ -5,11 +5,12 @@
         }
     </style>
 @endpush
-<div class="inline-block">
+<div class="inline-flex flex-row">
     <p class="inline">
         <span x-show="open">Hide</span>
         <span x-show="!open" x-cloak>Show</span>
     </p>
+    <span>&nbsp;</span>
     <p class="inline">
         <span>{{$text}}</span>
     </p>
