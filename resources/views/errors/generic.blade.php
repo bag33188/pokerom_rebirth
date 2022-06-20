7e9f0afcc21a91cb4d8243cfcadba33b0f31b769
bag33188 <bglatman@outlook.com>
2022-06-20 13:35:03 -0700

    fix

--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -5,7 +5,7 @@
                 Error
             </div>
             <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700 text-center">
-                <p>{!! $message !!}</p>
+                <p>{!! 'Oops, an unknown error occurred, please try again later.' !!}</p>
             </div>
         </div>
     </div>
