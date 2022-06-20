f1f52026e99ddcae635d89eb460cfe8b5ecd1c11
bag33188 <bglatman@outlook.com>
2022-06-20 15:36:15 -0700

    fix

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->text('two_factor_secret')
@@ -38,7 +38,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(array_merge([
