2e074c19641bbc5a48fbcf26d59123af9436d470
bag33188 <bglatman@outlook.com>
2022-06-19 19:32:11 -0700

    fix

--- resources/views/ui/show-hide.blade.php
+++ resources/views/ui/show-hide.blade.php
@@ -1,7 +1,14 @@
+@push('styles')
+    <style type="text/css">
+        [x-cloak] {
+            display: none;
+        }
+    </style>
+@endpush
 <div class="inline-block">
     <p class="inline">
         <span x-show="open">Hide</span>
-        <span x-show="!open">Show</span>
+        <span x-show="!open" x-cloak>Show</span>
     </p>
     <p class="inline">
         <span>{{$text}}</span>
