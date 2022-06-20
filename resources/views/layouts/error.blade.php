ee0fceffefec3459fa8f35ee610a973a3852f24e
bag33188 <bglatman@outlook.com>
2022-06-19 19:51:44 -0700

    fix

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <title>{{ config('app.name', 'Pok' . _EACUTE .'ROM') }}</title>
+    <title>{!! config('app.name', 'Pok' . _EACUTE .'ROM') !!}</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body class="h-full">
