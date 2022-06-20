17fdc13743aaf2d356f6942a9e956c3a3f6079c3
bag33188 <bglatman@outlook.com>
2022-06-19 20:41:18 -0700

    fix

--- app/Enums/FileTypesEnum.php
+++ app/Enums/FileTypesEnum.php
@@ -4,6 +4,8 @@ namespace App\Enums;
 
 enum FileTypesEnum: string
 {
+    // (generic) binary stream where file type is unknown
     case OCTET_STREAM = "application/octet-stream";
+    // unofficial mime type (custom http header)
     case X_BINARY = "application/x-binary";
 }
