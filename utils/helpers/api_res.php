f80839150d70bc79b82d490087f70875fcbb5345
Broccolini <bglatman@callutheran.edu>
2022-06-20 20:06:36 -0700

    fix

--- utils/helpers/api_res.php
+++ utils/helpers/api_res.php
@@ -1,6 +1,9 @@
 <?php
 
-function api_res(array $data, bool $success = true)
+use JetBrains\PhpStorm\ArrayShape;
+
+#[ArrayShape([0 => "array", 'success' => "bool"])]
+function api_res(array $data, bool $success = true): array
 {
     return [...$data, 'success' => $success];
 }
