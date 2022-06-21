84c4779203bef18a871f339620fadba821e1d1e4
Broccolini <bglatman@callutheran.edu>
2022-06-21 02:42:00 -0700

    fix

--- config/gridfs.php
+++ config/gridfs.php
@@ -9,11 +9,11 @@ return [
         'username' => env('DB_USERNAME_SECOND', 'brock'),
         'password' => env('DB_PASSWORD_SECOND', 'secret'),
         'auth' => [
-            'source' => env('DB_AUTHENTICATION_DATABASE'),
+            'source' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
             'mechanism' => env('DB_AUTHENTICATION_MECHANISM')
         ]
     ],
-    'bucketName' => env('DB_GFS_BUCKET_NAME'),
-    'chunkSize' => intval(env('DB_GFS_CHUNK_SIZE'), 16),
+    'bucketName' => env('DB_GFS_BUCKET_NAME', 'fs'),
+    'chunkSize' => intval(env('DB_GFS_CHUNK_SIZE', 0x3FC00), 16),
     'fileUploadPath' => storage_path('app/rom_files')
 ];
