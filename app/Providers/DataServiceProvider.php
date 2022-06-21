eb8c3030ba113ff521b623921dfb4f508fb5f285
Broccolini <bglatman@callutheran.edu>
2022-06-21 02:44:13 -0700

    fix

--- app/Providers/DataServiceProvider.php
+++ app/Providers/DataServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace App\Providers;
 
-use App\Interfaces\FileServiceInterface;
-use App\Interfaces\GameServiceInterface;
-use App\Interfaces\RomServiceInterface;
-use App\Interfaces\UserServiceInterface;
+use App\Interfaces\FileServiceInterface as IFileDataService;
+use App\Interfaces\GameServiceInterface as IGameDataService;
+use App\Interfaces\RomServiceInterface as IRomDataService;
+use App\Interfaces\UserServiceInterface as IUserDataService;
 use App\Services\FileDataService;
 use App\Services\GameDataService;
 use App\Services\RomDataService;
@@ -21,10 +21,10 @@ class DataServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(UserServiceInterface::class, UserDataService::class);
-        $this->app->bind(GameServiceInterface::class, GameDataService::class);
-        $this->app->bind(RomServiceInterface::class, RomDataService::class);
-        $this->app->bind(FileServiceInterface::class, FileDataService::class);
+        $this->app->bind(IUserDataService::class, UserDataService::class);
+        $this->app->bind(IGameDataService::class, GameDataService::class);
+        $this->app->bind(IRomDataService::class, RomDataService::class);
+        $this->app->bind(IFileDataService::class, FileDataService::class);
     }
 
     /**
