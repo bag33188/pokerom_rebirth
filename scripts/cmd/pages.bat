364b6b3def3fab467e19fb1fa555df2aaea14666
Broccolini <bglatman@callutheran.edu>
2022-06-20 21:13:16 -0700

    fix

---
+++ scripts/cmd/pages.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+START "" https://jetstream.laravel.com/2.x/building-your-app.html
+START "" https://laravel-livewire.com/docs/2.x/making-components
+START "" https://laravel.com/docs/9.x/eloquent-resources
+START "" https://laravel.com/docs/9.x/eloquent-serialization
+
+EXIT /B 0
