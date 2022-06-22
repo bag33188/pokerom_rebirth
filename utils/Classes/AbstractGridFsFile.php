fb62754ac4fd045280c04d5c7b2d47ca0733a153
bag33188 <bglatman@outlook.com>
2022-06-22 16:53:30 -0700

    fix

--- utils/Classes/AbstractGridFsFile.php
+++ utils/Classes/AbstractGridFsFile.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Jenssegers\Mongodb\Eloquent\Model as MongoDbModel;
 use MongoDB\BSON\ObjectId;
 
+/**
+ * Laravel IDEA Helper does not support MongoDB objects (at least not very well).
+ * The purpose of this abstract class is to provider easier hinting and implementation
+ * of this app's {@link \App\Models\File File model}.
+ *
+ * @see \App\Models\File File Model
+ */
 abstract class AbstractGridFsFile extends MongoDbModel
 {
     public readonly string $_id;
