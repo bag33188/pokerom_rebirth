64783d37297012ffd059366641e4760214197ed7
bag33188 <bglatman@outlook.com>
2022-06-22 16:47:56 -0700

    fix

--- utils/Classes/AbstractGridFsFile.php
+++ utils/Classes/AbstractGridFsFile.php
@@ -3,10 +3,10 @@
 namespace Utils\Classes;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as MongoDbModel;
 use MongoDB\BSON\ObjectId;
 
-abstract class AbstractGridFsFile extends Model
+abstract class AbstractGridFsFile extends MongoDbModel
 {
     public readonly string $_id;
     public readonly int $chunkSize;
