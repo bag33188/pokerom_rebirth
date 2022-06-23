f6a39dbafd1beddee6a48954c83be867fcd0be9c
bag33188 <bglatman@outlook.com>
2022-06-22 18:41:35 -0700

    fix

--- utils/functions/jsondata.php
+++ utils/functions/jsondata.php
@@ -6,6 +6,7 @@ use Utils\Classes\JsonDataResponse;
 if (!function_exists('jsondata')) {
     function jsondata(array $data, int $code, array $headers = []): JsonResponse
     {
+        // (new JsonDataResponse($data, $code, $headers))();
         $response = new JsonDataResponse($data, $code, $headers);
         return $response();
     }
