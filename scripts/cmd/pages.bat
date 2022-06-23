152f272db44cf1f46147553c65641f2546523761
Broccolini <bglatman@callutheran.edu>
2022-06-22 23:24:38 -0700

    fix

--- scripts/cmd/pages.bat
+++ scripts/cmd/pages.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 
+START "" https://github.com/bag33188/pokerom_rebirth
 START "" https://jetstream.laravel.com/2.x/building-your-app.html
 START "" https://laravel-livewire.com/docs/2.x/making-components
 START "" https://laravel.com/docs/9.x/eloquent-resources
