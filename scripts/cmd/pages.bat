9e81ed21e505e805983a6c3687a70f1544d14fb6
Broccolini <bglatman@callutheran.edu>
2022-06-22 23:33:53 -0700

    fix

--- scripts/cmd/pages.bat
+++ scripts/cmd/pages.bat
@@ -1,11 +1,10 @@
 @ECHO OFF
 
-SET repo=https://github.com/bag33188/pokerom_rebirth
-
-START "" %repo%
-START "" https://jetstream.laravel.com/2.x/building-your-app.html
-START "" https://laravel-livewire.com/docs/2.x/making-components
-START "" https://laravel.com/docs/9.x/eloquent-resources
-START "" https://laravel.com/docs/9.x/eloquent-serialization
-
+:OPEN_PAGES
+    SET REPO_URL=https://github.com/bag33188/pokerom_rebirth
+    START "" %REPO_URL%
+    START "" https://jetstream.laravel.com/2.x/building-your-app.html
+    START "" https://laravel-livewire.com/docs/2.x/making-components
+    START "" https://laravel.com/docs/9.x/eloquent-resources
+    START "" https://laravel.com/docs/9.x/eloquent-serialization
 EXIT /B 0
