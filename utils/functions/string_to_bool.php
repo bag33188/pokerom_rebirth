813a66e5a03a9d746f76ebe4de8615e55f4d73ce
Broccolini <bglatman@callutheran.edu>
2022-06-22 23:36:54 -0700

    fix

--- utils/functions/string_to_bool.php
+++ utils/functions/string_to_bool.php
@@ -1,6 +1,10 @@
 <?php
 
 if (!function_exists('string_to_bool')) {
+    /**
+     * @param string $value
+     * @return bool
+     */
     function string_to_bool(string $value): bool
     {
         return filter_var($value, FILTER_VALIDATE_BOOL);
