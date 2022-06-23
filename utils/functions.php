01d2f83fc41d3ce3a528d4cc8a20208b854482a8
bag33188 <bglatman@outlook.com>
2022-06-22 19:40:03 -0700

    fix

--- utils/functions.php
+++ utils/functions.php
@@ -5,9 +5,9 @@
 $functions_path = 'functions'; // preg_replace("/\\\+/", "/", join('/', [__DIR__, 'functions']));
 
 require_once "$functions_path/str_capitalize.php";
-require_once "$functions_path/number_to_roman.php";
 require_once "$functions_path/parse_date_as_readable_string.php";
 require_once "$functions_path/string_to_bool.php";
 require_once "$functions_path/jsondata.php";
+include_once "$functions_path/number_to_roman.php";
 
 //--- END GLOBAL HELPER FUNCTIONS ---//
