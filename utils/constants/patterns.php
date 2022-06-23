1187e00473ea3c4b54609ad336ea8d6f7913ea51
bag33188 <bglatman@outlook.com>
2022-06-23 15:46:16 -0700

    fix

--- utils/constants/patterns.php
+++ utils/constants/patterns.php
@@ -3,12 +3,12 @@
 /** rom filename regular expression */
 const ROM_FILE_NAME_PATTERN = /** @lang RegExp */
 "/^([\w\d\-_]{3,32})\.(3ds|xci|nds|gbc|gb|gba)$/i";
+/** rom file extension regular expression */
+const ROM_FILE_TYPES_PATTERN = /** @lang RegExp */
+"/\.(gba|gbc|gb|nds|xci|3ds)$/i";
 /** game name regular expression */
 const GAME_NAME_PATTERN = /** @lang RegExp */
 "/^Pokemon\s.+$/";
 /** rom name regular expression */
 const ROM_NAME_PATTERN = /** @lang RegExp */
 "/^[\w_-]+$/i";
-/** rom file extension regular expression */
-const ROM_FILE_TYPES_PATTERN = /** @lang RegExp */
-"/\.(gba|gbc|gb|nds|xci|3ds)$/i";
