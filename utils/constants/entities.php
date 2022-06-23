750cf163078ee009547891b45d51000a006884d6
Broccolini <bglatman@callutheran.edu>
2022-06-22 23:48:42 -0700

    fi

--- utils/constants/entities.php
+++ utils/constants/entities.php
@@ -6,5 +6,5 @@ const FILE_FORM_KEY = 'file';
 const API_TOKEN_KEY = 'auth_token';
 /** the special _**`e`**_ character in the Pokemon logo */
 const _EACUTE = "\u{00E9}";
-/** uppercase version of the special _**`E`**_ character in the Pokemon logo */
+/** uppercase version of {@see _EACUTE} */
 const _U_EACUTE = "\u{00C9}";
