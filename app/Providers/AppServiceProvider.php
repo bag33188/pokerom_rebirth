a07d81c68d2bc67a70dcaaa2c71c6b3f033e50b6
bag33188 <bglatman@outlook.com>
2022-06-23 14:41:00 -0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,20 @@
 namespace App\Providers;
 
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public readonly bool $ideHelperActive;
+    protected bool $isLocal = true;
+
+    public function __construct(Application $app)
+    {
+        parent::__construct($app);
+        if (!$this->app->isLocal()) $this->isLocal = false;
+    }
+
     /**
      * Register any application services.
      *
@@ -14,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->isLocal()) {
+        if ($this->isLocal) {
             $this->app->register(IdeHelperServiceProvider::class);
         }
     }
@@ -26,6 +36,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-//        $this->register();
+        if ($this->isLocal) {
+            $this->ideHelperActive = true;
+        }
     }
 }
