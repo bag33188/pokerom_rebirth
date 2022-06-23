d223b704af6fd9541e1bd549cddf09b5ca4d5b42
bag33188 <bglatman@outlook.com>
2022-06-22 18:40:22 -0700

    fix

--- app/Services/GameDataDataService.php
+++ app/Services/GameDataDataService.php
@@ -5,12 +5,11 @@ namespace App\Services;
 use App\Events\GameCreated;
 use App\Interfaces\GameDataServiceInterface;
 use App\Models\Game;
-use Illuminate\Database\Eloquent\Model;
 use RomRepo;
 
 class GameDataDataService implements GameDataServiceInterface
 {
-    public function createGameFromRomId(int $romId, array $data): Model|Game
+    public function createGameFromRomId(int $romId, array $data): Game
     {
         $rom = RomRepo::findRomIfExists($romId);
         $game = $rom->game()->create($data);
