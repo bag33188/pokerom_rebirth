858efc2c5e1214e046105cc7e0a7f3d1f9aea318
Broccolini <bglatman@callutheran.edu>
2022-06-23 20:45:21 -0700

    fix

--- resources/views/livewire/roms/edit.blade.php
+++ resources/views/livewire/roms/edit.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <h2>Edit {{$rom->getRomFileName()}}</h2>
     </x-slot>
-    <x-jet-validation-errors class="mb-4"/>
+    <x-jet-validation-errors class="mb-4" />
 
     @if (session('status'))
         <div class="mb-4 font-medium text-sm text-green-600">
@@ -14,9 +14,9 @@
         @method('PATCH')
 
         <div>
-            <x-jet-label for="romName" value="{{ __('Rom Name') }}"/>
+            <x-jet-label for="romName" value="{{ __('Rom Name') }}" />
             <x-jet-input id="romName" class="block mt-1 w-full" type="text" name="rom_name" :value="old('rom_name')"
-                         required autofocus/>
+                         required autofocus />
         </div>
 
         <x-jet-button class="ml-4">
