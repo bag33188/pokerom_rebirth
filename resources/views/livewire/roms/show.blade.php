0a23b1ea00a072e7feda21d90379196e5a549018
bag33188 <bglatman@outlook.com>
2022-06-23 19:37:51 -0700

    fix

--- resources/views/livewire/roms/show.blade.php
+++ resources/views/livewire/roms/show.blade.php
@@ -2,9 +2,8 @@
     <ul>
         <li>{{$rom->rom_name}}</li>
         <li>{{RomRepo::getRomReadableSize($rom->rom_size)}}</li>
-        <li>{{@$rom->game->game_name ?? 'no game'}}</li>
-        <li></li>
-        <li></li>
+        <li>{{@$rom->game->game_name . ' Version' ?? 'no game'}}</li>
+        <li>{{@$rom->file->filename ?? 'no file'}}</li>
         <li></li>
     </ul>
 </div>
