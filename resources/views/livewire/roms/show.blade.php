1289e540db9b4b24f3a6522825d90bfe4e184fd4
Broccolini <bglatman@callutheran.edu>
2022-06-23 22:28:50 -0700

    fix

--- resources/views/livewire/roms/show.blade.php
+++ resources/views/livewire/roms/show.blade.php
@@ -1,11 +1,12 @@
 @php
     $editBtnClasses = 'class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition"';
+    $isAdmin = Auth::user()->isAdmin();
 @endphp
 <div class="p-2.5">
     <x-slot name="header">
         <h2 class="text-center">{{$rom->getRomFileName()}} Information</h2>
     </x-slot>
-    <div class="w-full grid grid-cols-2 grid-rows-2 gap-y-4">
+    <div class="w-full grid grid-cols-2 grid-rows-{{$isAdmin ? 2 : 1}} gap-y-4">
         <ul class="bg-white rounded-lg border border-gray-200 text-gray-900 col-span-full row-start-1 row-end-1">
             <li class="px-6 py-2 border-b border-gray-200 w-full">{{$rom->rom_name}}</li>
             <li class="px-6 py-2 border-b border-gray-200 w-full">{{RomRepo::getRomReadableSize($rom->rom_size)}}</li>
@@ -18,7 +19,7 @@
             @endif
         </ul>
 
-        @if(Auth::user()->isAdmin())
+        @if($isAdmin)
             <div class="col-start-2 col-end-2 row-start-2 row-end-2 justify-self-end">
                 <livewire:roms.delete :rom-id="$romId" />
             </div>
