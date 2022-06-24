912e16b6de739cec32654f08bba4f4015fcbd8de
Broccolini <bglatman@callutheran.edu>
2022-06-23 22:43:07 -0700

    fix

--- resources/views/livewire/roms/show.blade.php
+++ resources/views/livewire/roms/show.blade.php
@@ -1,5 +1,11 @@
 @php
-    $editBtnClasses = 'class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition"';
+    $editBtnClasses = <<<'EOS'
+    class="inline-flex items-center px-4 py-2 bg-gray-800 border
+    border-transparent rounded-md font-semibold text-xs text-white
+    uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900
+    focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300
+    disabled:opacity-25 transition"
+    EOS;
     $isAdmin = Auth::user()->isAdmin();
 @endphp
 <div class="p-2.5">
