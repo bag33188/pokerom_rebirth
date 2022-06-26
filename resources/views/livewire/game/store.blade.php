7d7d78d9959f459e7d8374a4ddad138191441a2e
bag33188 <bglatman@outlook.com>
2022-06-26 13:27:12 -0700

    fix

--- resources/views/livewire/game/store.blade.php
+++ resources/views/livewire/game/store.blade.php
@@ -1,14 +1,14 @@
-@php
-    $availableRoms=GameRepo::getAllRomsWithNoGame();
-    $availableRomsCount = count($availableRoms);
-@endphp
 <div class="container">
-    @for($i = 0; $i < $availableRomsCount; $i++)
-        @php
-            $rom=$availableRoms[$i];
-            echo $rom->rom_name;
-        @endphp
-    @endfor
-    {!! "<br/>" !!}
-    {{var_export(GameRepo::getAllRomsWithNoGame())}}
+    @if($availableRoms > 0)
+        @for($i = 0; $i < $availableRomsCount; $i++)
+            @php
+                $rom = $availableRoms[$i];
+                echo $rom->rom_name;
+            @endphp
+        @endfor
+        {!! "<br/>" !!}
+        {{var_export(GameRepo::getAllRomsWithNoGame())}}
+    @else
+        <h3>No available roms to add a game to :(</h3>
+    @endif
 </div>
