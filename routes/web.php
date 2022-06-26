2f394d2464ded8d60b16b621894d73203dab13d9
bag33188 <bglatman@outlook.com>
2022-06-26 12:43:43 -0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Livewire\Game\Store as GameStore;
 use App\Http\Controllers\www\{HomeController, WelcomeController};
 use App\Http\Livewire\Game\Delete as GameDelete;
 use App\Http\Livewire\Game\Edit as GameEdit;
 use App\Http\Livewire\Game\Index as GameIndex;
 use App\Http\Livewire\Game\Show as GameShow;
+use App\Http\Livewire\Game\Store as GameStore;
 use App\Http\Livewire\Rom\Delete as RomDelete;
 use App\Http\Livewire\Rom\Edit as RomEdit;
 use App\Http\Livewire\Rom\Index as RomIndex;
@@ -31,15 +31,19 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/dashboard', [HomeController::class, 'renderDashboard'])->name('dashboard');
-    Route::get('/roms', RomIndex::class)->name('roms.index');
-    Route::get('/roms/show/{romId}', RomShow::class)->name('roms.show');
-    Route::get('/roms/edit/{romId}', RomEdit::class)->name('roms.edit')->middleware('admin');
-    Route::put('/roms/update/{romId}', [RomEdit::class, 'update'])->name('roms.update');
-    Route::delete('/roms/delete/{romId}', [RomDelete::class, 'delete'])->name('roms.delete');
-    Route::get('/games', GameIndex::class)->name('games.index');
-    Route::get('/games/add', GameStore::class)->name('games.add')->middleware('admin');
-    Route::get('/games/show/{gameId}', GameShow::class)->name('games.show');
-    Route::get('/games/edit/{gameId}', GameEdit::class)->name('games.edit')->middleware('admin');
-    Route::put('/games/update/{gameId}', [GameEdit::class, 'update'])->name('games.update');
-    Route::delete('/games/delete/{gameId}', [GameDelete::class, 'delete'])->name('games.delete');
+    Route::prefix('roms')->group(function () {
+        Route::get('/', RomIndex::class)->name('roms.index');
+        Route::get('/show/{romId}', RomShow::class)->name('roms.show');
+        Route::get('/edit/{romId}', RomEdit::class)->name('roms.edit')->middleware('admin');
+        Route::put('/update/{romId}', [RomEdit::class, 'update'])->name('roms.update');
+        Route::delete('/delete/{romId}', [RomDelete::class, 'delete'])->name('roms.delete');
+    });
+    Route::prefix('games')->group(function () {
+        Route::get('/', GameIndex::class)->name('games.index');
+        Route::get('/add', GameStore::class)->name('games.add')->middleware('admin');
+        Route::get('/show/{gameId}', GameShow::class)->name('games.show');
+        Route::get('/edit/{gameId}', GameEdit::class)->name('games.edit')->middleware('admin');
+        Route::put('/update/{gameId}', [GameEdit::class, 'update'])->name('games.update');
+        Route::delete('/delete/{gameId}', [GameDelete::class, 'delete'])->name('games.delete');
+    });
 });
