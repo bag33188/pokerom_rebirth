f3986ef9623434b2af31a947946833bc87de70ba
bag33188 <bglatman@outlook.com>
2022-06-26 12:36:31 -0700

    fix

--- resources/views/livewire/game/store.blade.php
+++ resources/views/livewire/game/store.blade.php
@@ -4,7 +4,10 @@
 @endphp
 <div>
     @for($i = 0; $i < $availableRomsCount; $i++)
-        {{$availableRoms[$i]->rom_name}}
+        @php
+            $rom=$availableRoms[$i];
+            echo $rom->rom_name;
+        @endphp
     @endfor
     {!! "<br/>" !!}
     {{var_export($this->getAvailableRoms())}}
