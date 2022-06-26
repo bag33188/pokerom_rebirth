1da7ebb06c371e3c8e7a3ac7b43957a6f9dd7062
bag33188 <bglatman@outlook.com>
2022-06-26 12:52:05 -0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         <!-- Styles -->
         {{--<link rel="stylesheet" href="{{ mix('css/app.css') }}">--}}
         <link rel="stylesheet" <?= 'type="text/css"'; ?> href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" <?= 'type="text/css"'; ?> href="{{ asset('css/scrollbar.css') }}">
         @stack('styles')
 
         @livewireStyles
