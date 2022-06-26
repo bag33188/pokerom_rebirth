5b871f7b0accb3ddf3de0d15d1658114fb516222
bag33188 <bglatman@outlook.com>
2022-06-26 11:33:03 -0700

    fix

--- app/Http/Livewire/Game/Edit.php
+++ app/Http/Livewire/Game/Edit.php
@@ -5,6 +5,9 @@ namespace App\Http\Livewire\Game;
 use App\Http\Requests\UpdateGameRequest;
 use App\Models\Game;
 use GameRepo;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Edit extends Component
@@ -18,7 +21,7 @@ class Edit extends Component
         $this->game = GameRepo::findGameIfExists($gameId);
     }
 
-    public function render()
+    public function render(): Factory|View|Application
     {
         return view('livewire.game.edit', ['gameId' => $this->gameId]);
     }
