2d33459b7ee952ea65633fd3bee18eeb2d56725a
bag33188 <bglatman@outlook.com>
2022-06-27 07:13:52 -0700

    fix

---
+++ app/Http/Livewire/RomFile/Upload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\RomFile;
+
+use Livewire\Component;
+
+class Upload extends Component
+{
+    public function render()
+    {
+        return view('livewire.rom-file.upload');
+    }
+}
