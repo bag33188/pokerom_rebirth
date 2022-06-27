dc0d4d1f326809038a1ac38142a3c55682beec19
Broccolini <bglatman@callutheran.edu>
2022-06-27 11:23:07 -0700

    fix

---
+++ resources/js/capitalize.js
@@ -0,0 +1,35 @@
+/**
+ * @summary Capitalizes a string
+ * @description By default this method capitalizes only the first word in a string.
+ * @param {boolean} [deep] whether to capitalize all words in the string or not (defaults to false).
+ * @param {number} [depth] amount of words to capitalize in string
+ * @param {string|null} [separator] Set to any string value if you wish to distinguish the words in your string by a character other than space
+ * @returns {string|null} capitalized string
+ */
+String.prototype.capitalize = function (
+    deep = false,
+    depth = 0,
+    separator = null
+) {
+    let val = this.trim();
+    if (!val.length) return null;
+    const wordsSeparator = separator ? separator : " ";
+    let strArr = val.split(wordsSeparator);
+    if (deep === true && depth > 0) {
+        let strWordCount = strArr.length;
+        if (depth > strWordCount) depth = strWordCount;
+        let i = 0;
+        do {
+            strArr[i] =
+                strArr[i][0].toUpperCase() + strArr[i].slice(1).toLowerCase();
+            i++;
+        } while (i < depth);
+        return strArr.join(wordsSeparator);
+    } else if (deep === true && depth === 0) {
+        return strArr
+            .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+            .join(wordsSeparator);
+    } else {
+        return val[0].toUpperCase() + val.slice(1).toLowerCase();
+    }
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,10 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-    ]).css('resources/css/punch.css', 'public/css');
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .css("resources/css/punch.css", "public/css")
+    .js("resources/js/capitalize.js", "public/js/modules");
 
 if (mix.inProduction()) {
     mix.version();
