c60f1dd5d1ea2f9c0d76310e2c13dc0d2a4d7383
bag33188 <bglatman@outlook.com>
2022-06-27 17:21:40 -0700

    fix

--- utils/Modules/GridFS/AbstractGridFsBucket.php
+++ utils/Modules/GridFS/AbstractGridFsBucket.php
@@ -7,11 +7,34 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 abstract class AbstractGridFsBucket extends FileBucket implements GridFileResourcing
 {
+    /**
+     * Size of chunks interval(s) to download
+     *
+     * @var int
+     */
     protected const DOWNLOAD_CHUNK_SIZE = 0x3FC00;
 
+    /**
+     * Upload file to GridFS
+     *
+     * @param UploadedFile $file
+     * @return void
+     */
     abstract public function upload(UploadedFile $file): void;
 
+    /**
+     * Download File from GridFS
+     *
+     * @param string $fileId
+     * @return void
+     */
     abstract public function download(string $fileId): void;
 
+    /**
+     * Delete GridFS File Data
+     *
+     * @param Model $file
+     * @return void
+     */
     abstract public function destroy(Model $file): void;
 }
--- utils/Modules/GridFS/GridFileResourcing.php
+++ utils/Modules/GridFS/GridFileResourcing.php
@@ -6,9 +6,24 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 interface GridFileResourcing
 {
+    /**
+     * Get filename string
+     *
+     * @return string
+     */
     public function getFilename(): string;
 
+    /**
+     * Get extension of file name
+     *
+     * @return string
+     */
     public function getFileExt(): string;
 
+    /**
+     * Get file resource model (non-gfs)
+     *
+     * @return Model
+     */
     public function getFileDocument(): Model;
 }
