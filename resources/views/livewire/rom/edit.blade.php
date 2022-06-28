dd302c81276c78e64fb6e44e34bbd85740322310
bag33188 <bglatman@outlook.com>
2022-06-28 14:21:55 -0700

    fix

--- resources/views/livewire/rom/edit.blade.php
+++ resources/views/livewire/rom/edit.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
     <div class="p-3">
         @if (session()->has('message'))
-            <div class="bg-gray-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
+            <div class="bg-gray-100 border-t-4 border-red-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
                  role="alert">
                 <div class="flex">
                     <div>
