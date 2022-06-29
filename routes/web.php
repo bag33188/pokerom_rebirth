237aed2362693425210f12bc4c071085ba20e4ea
bag33188 <bglatman@outlook.com>
2022-06-28 17:03:24 -0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,10 @@
 
 use App\Http\Controllers\www\{HomeController, RomFileController, WelcomeController};
 use App\Http\Livewire\Game\Create as CreateGame;
-use App\Http\Livewire\Game\Delete as DeleteGame;
 use App\Http\Livewire\Game\Edit as EditGame;
 use App\Http\Livewire\Game\Index as IndexGame;
 use App\Http\Livewire\Game\Show as ShowGame;
 use App\Http\Livewire\Rom\Create as CreateRom;
-use App\Http\Livewire\Rom\Delete as DeleteRom;
 use App\Http\Livewire\Rom\Edit as EditRom;
 use App\Http\Livewire\Rom\Index as IndexRom;
 use App\Http\Livewire\Rom\Show as ShowRom;
@@ -31,26 +29,26 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
+    // laravel routes
     Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
-    Route::prefix('roms')->group(function () {
-        Route::get('/', IndexRom::class)->name('roms.index');
-        Route::get('/create', CreateRom::class)->name('roms.create')->middleware('admin');
-        Route::get('/show/{romId}', ShowRom::class)->name('roms.show');
-        Route::get('/edit/{romId}', EditRom::class)->name('roms.edit')->middleware('admin');
-        Route::delete('/delete/{romId}', [DeleteRom::class, 'delete'])->name('roms.delete');
-    });
-    Route::prefix('games')->group(function () {
-        Route::get('/', IndexGame::class)->name('games.index');
-        Route::get('/create', CreateGame::class)->name('games.create')->middleware('admin');
-        Route::get('/show/{gameId}', ShowGame::class)->name('games.show');
-        Route::get('/edit/{gameId}', EditGame::class)->name('games.edit')->middleware('admin');
-        Route::delete('/delete/{gameId}', [DeleteGame::class, 'delete'])->name('games.delete');
-    });
     Route::prefix('files')->group(function () {
         Route::get('/', [RomFileController::class, 'index'])->name('files.index');
         Route::get('/info/{romFile}', [RomFileController::class, 'show'])->name('files.show');
         Route::get('/create', [RomFileController::class, 'create'])->name('files.create')->middleware('admin');
         Route::post('/store', [RomFileController::class, 'store'])->name('files.store');
         Route::delete('/delete/{romFile}', [RomFileController::class, 'destroy'])->name('files.delete');
+    });
+    // livewire routes
+    Route::prefix('roms')->group(function () {
+        Route::get('/', IndexRom::class)->name('roms.index');
+        Route::get('/create', CreateRom::class)->name('roms.create')->middleware('admin');
+        Route::get('/show/{romId}', ShowRom::class)->name('roms.show');
+        Route::get('/edit/{romId}', EditRom::class)->name('roms.edit')->middleware('admin');
+    });
+    Route::prefix('games')->group(function () {
+        Route::get('/', IndexGame::class)->name('games.index');
+        Route::get('/create', CreateGame::class)->name('games.create')->middleware('admin');
+        Route::get('/show/{gameId}', ShowGame::class)->name('games.show');
+        Route::get('/edit/{gameId}', EditGame::class)->name('games.edit')->middleware('admin');
     });
 });
