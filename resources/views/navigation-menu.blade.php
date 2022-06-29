ba53a2dd9fc321b3bc7c9f407335daae66f84805
bag33188 <bglatman@outlook.com>
2022-06-28 17:00:10 -0700

    update code base

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -44,7 +44,7 @@
                         </x-jet-nav-link>
                     </div>
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-jet-nav-link href="{{ route('files.store') }}" :active="request()->routeIs('files.store')">
+                        <x-jet-nav-link href="{{ route('files.create') }}" :active="request()->routeIs('files.create')">
                             {{ __('Add ROM File') }}
                         </x-jet-nav-link>
                     </div>
