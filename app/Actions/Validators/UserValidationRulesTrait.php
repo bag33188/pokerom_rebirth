1fec97f407309fd3b180813b6e0df13182d3bb24
Broccolini <bglatman@callutheran.edu>
2022-06-29 05:11:55 -0700

    update code base

--- app/Actions/Validators/UserValidationRulesTrait.php
+++ app/Actions/Validators/UserValidationRulesTrait.php
@@ -4,19 +4,23 @@ namespace App\Actions\Validators;
 
 use App\Rules\MaxLength;
 use App\Rules\MinLength;
+use JetBrains\PhpStorm\Pure;
 
 trait UserValidationRulesTrait
 {
+    #[Pure]
     protected function userNameRules(array $rules = ['required']): array
     {
         return [...$rules, 'string', new MinLength(MIN_USER_NAME), new MaxLength(MAX_USER_NAME)];
     }
 
+    #[Pure]
     protected function userEmailRules(array $rules = ['required']): array
     {
         return [...$rules, 'string', 'email', new MaxLength(MAX_USER_EMAIL)];
     }
 
+    #[Pure]
     protected function userPasswordRules(array $rules = ['required']): array
     {
         return [...$rules, new MaxLength(MAX_USER_PASSWORD), new MinLength(MIN_USER_PASSWORD)];
