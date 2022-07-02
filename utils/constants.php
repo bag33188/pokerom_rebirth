de59aa74f100951a4964cca854dbe5f60e3a4383
bag33188 <bglatman@outlook.com>
2022-07-02 11:25:35 -0700

    update code base

--- utils/constants.php
+++ utils/constants.php
@@ -2,13 +2,13 @@
 
 //--- GLOBAL CONSTANTS ---//
 
-$constants_path = "constants";
+$constants_pathname = "constants";
 
-require_once "$constants_path/enums.php";
-require_once "$constants_path/patterns.php";
-require_once "$constants_path/entities.php";
-require_once "$constants_path/sizes.php";
-require_once "$constants_path/lengths.php";
-require_once "$constants_path/chars.php";
+require_once "$constants_pathname/enums.php";
+require_once "$constants_pathname/patterns.php";
+require_once "$constants_pathname/entities.php";
+require_once "$constants_pathname/sizes.php";
+require_once "$constants_pathname/lengths.php";
+require_once "$constants_pathname/chars.php";
 
 //--- END GLOBAL CONSTANTS ---//
--- utils/functions.php
+++ utils/functions.php
@@ -2,13 +2,13 @@
 
 //--- GLOBAL HELPER FUNCTIONS ---//
 
-$functions_path = 'functions'; # preg_replace("/\\\+/", "/", join('/', [__DIR__, 'functions']));
+$functions_pathname = 'functions';
 
-require_once "$functions_path/str_capitalize.php";
-require_once "$functions_path/parse_date_as_readable_string.php";
-require_once "$functions_path/string_to_bool.php";
-require_once "$functions_path/jsondata.php";
-require_once "$functions_path/number_to_roman.php";
-require_once "$functions_path/eacute_poke_conversion.php";
+require_once "$functions_pathname/str_capitalize.php";
+require_once "$functions_pathname/parse_date_as_readable_string.php";
+require_once "$functions_pathname/string_to_bool.php";
+require_once "$functions_pathname/jsondata.php";
+require_once "$functions_pathname/number_to_roman.php";
+require_once "$functions_pathname/eacute_poke_conversion.php";
 
 //--- END GLOBAL HELPER FUNCTIONS ---//
