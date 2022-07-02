866583a0b887c3ff79495ac310845442fa98fbb6
bag33188 <bglatman@outlook.com>
2022-07-02 11:21:27 -0700

    update code base

---
+++ utils/constants/chars.php
@@ -0,0 +1,6 @@
+<?php
+
+/** the special _**&eacute;**_ character in the Pokemon logo */
+const _EACUTE = "\u{00E9}";
+/** uppercase version of {@see _EACUTE}, **&Eacute;** */
+const _U_EACUTE = "\u{00C9}";
--- utils/constants/entities.php
+++ utils/constants/entities.php
@@ -4,7 +4,5 @@
 const ROM_FILES_DIRNAME = 'rom_files';
 /** the key name for sanctum personal access token */
 const API_TOKEN_KEY = 'auth_token';
-/** the special _**`e`**_ character in the Pokemon logo */
-const _EACUTE = "\u{00E9}";
-/** uppercase version of {@see _EACUTE} */
-const _U_EACUTE = "\u{00C9}";
+
+include_once "chars.php";
