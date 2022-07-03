86476ccce15bac32d229516763d07085b7911508
bag33188 <bglatman@outlook.com>
2022-07-03 11:05:01 -0700

    update code base

--- resources/views/rom-file/show.blade.php
+++ resources/views/rom-file/show.blade.php
@@ -9,7 +9,30 @@
             <x-list-item>Filesize: {{$romFile->length}} Bytes</x-list-item>
             <x-list-item>Chunk Size: {{$romFile->chunkSize}}</x-list-item>
             <x-list-item>MD5 Hash: {{$romFile->md5}}</x-list-item>
-            <x-list-item>@include('rom-file.delete', $romFile)</x-list-item>
+            <x-list-item>
+                @php
+                    function getRomFileDownloadUrl(string $fileId): string
+                    {
+                       $baseUrl = "/public/api";
+                       $baseFilesEndpoint = "files/grid/$fileId/download";
+                       if (App::isLocal()) return "$baseUrl/dev/$baseFilesEndpoint";
+                       return "$baseUrl/$baseFilesEndpoint";
+                    }
+                @endphp
+                <div class="inline-flex flex-row justify-between w-full">
+                    <span class="order-0">
+                        <a
+                            class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                            href="{{getRomFileDownloadUrl($romFile->_id)}}"
+                            target="_self"
+                            title="{{$romFile->filename}}">
+                                @include('partials._download-icon')
+                                <span>DOWNLOAD</span>
+                        </a>
+                    </span>
+                    <span class="order-1">@include('rom-file.delete', $romFile)</span>
+                </div>
+            </x-list-item>
         </x-list-group>
     </div>
 </x-app-layout>
