1f9c6276e7ee8e4189f172458b04bd50e7f687cc
bag33188 <bglatman@outlook.com>
2022-07-02 22:06:27 -0700

    update code base

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8"/>
-    <title>{{ config('app.name', unicode_eacute('PokeROM')) }}</title>
-    <link rel="stylesheet" <?= 'type="text/css"' ?> href="{{asset('css/app.css')}}"/>
-</head>
-<body class="h-full">
-<div class="bg-white">
-    <div class="flex h-full flex-col justify-center items-center">
-        {{$slot}}
-    </div>
-</div>
-</body>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>{{ config('app.name', unicode_eacute('PokeROM')) }}</title>
+        <link rel="stylesheet" <?= 'type="text/css"' ?> href="{{mix('css/app.css')}}"/>
+    </head>
+    <body class="h-full">
+        <div class="bg-white">
+            <div class="flex h-full flex-col justify-center items-center">
+                {{$slot}}
+            </div>
+        </div>
+    </body>
 </html>
