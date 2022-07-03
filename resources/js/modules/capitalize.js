368ca3b5c0c3a306fff15340d558472e30b1473c
bag33188 <bglatman@outlook.com>
2022-07-02 21:57:54 -0700

    update code base

--- resources/js/modules/capitalize.js
+++ resources/js/modules/capitalize.js
@@ -3,18 +3,17 @@
  * @description By default this method capitalizes only the first word in a string.
  * @param {boolean} [deep] whether to capitalize all words in the string or not (defaults to false).
  * @param {number} [depth] amount of words to capitalize in string
- * @param {string|null} [separator] Set to any string value if you wish to distinguish the words in your string by a character other than space
+ * @param {string} [separator] Set to any string value if you wish to distinguish the words in your string by a character other than space (default: space char)
  * @returns {string|null} capitalized string
  */
 String.prototype.capitalize = function (
     deep = false,
     depth = 0,
-    separator = null
+    separator = "\u0020"
 ) {
     let strVal = this.trim();
     if (!strVal.length) return null;
-    const wordsSeparator = separator ? separator : " ";
-    let strArr = strVal.split(wordsSeparator);
+    let strArr = strVal.split(separator);
     if (deep === true && depth > 0) {
         let strWordCount = strArr.length;
         if (depth > strWordCount) depth = strWordCount;
@@ -24,11 +23,11 @@ String.prototype.capitalize = function (
                 strArr[i][0].toUpperCase() + strArr[i].slice(1).toLowerCase();
             i++;
         } while (i < depth);
-        return strArr.join(wordsSeparator);
+        return strArr.join(separator);
     } else if (deep === true && depth === 0) {
         return strArr
             .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-            .join(wordsSeparator);
+            .join(separator);
     } else {
         return strVal[0].toUpperCase() + strVal.slice(1).toLowerCase();
     }
