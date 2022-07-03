fd55f8b47f6bce0901260a0f1bb35e8b001d7eb8
bag33188 <bglatman@outlook.com>
2022-07-03 09:50:08 -0700

    update code base

--- resources/js/Pages/Dashboard/index.js
+++ resources/js/Pages/Dashboard/index.js
@@ -1,12 +1,19 @@
+const checkIfNodeIsComment = (node) => {
+    const COMMENT_NODE_TYPE = 8;
+    return node.nodeType === COMMENT_NODE_TYPE;
+};
+
+const createWelcomeMessage = (text) => {
+    const capOpts = new CapitalizationOptions(true);
+    return document.createTextNode(text.capitalize(capOpts));
+};
+
 const loadWelcomeContent = (username) => {
     const welcomeUsername = document.getElementById("welcome-username");
     const jsInsertHtmlComment = welcomeUsername.childNodes[0];
     const welcomeMessage = `welcome, ${username}!`;
-    const capOpts = new CapitalizationOptions(true);
-    let welcomeText = document.createTextNode(
-        welcomeMessage.capitalize(capOpts)
-    );
-    if (jsInsertHtmlComment.nodeType === 8) {
+    const welcomeText = createWelcomeMessage(welcomeMessage);
+    if (checkIfNodeIsComment(jsInsertHtmlComment)) {
         welcomeUsername.replaceChild(welcomeText, jsInsertHtmlComment);
     } else {
         welcomeUsername.appendChild(welcomeText);
