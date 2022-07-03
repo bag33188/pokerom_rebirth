3d33bf3e417ec5d0c35920a5f6926c341abdfcbd
bag33188 <bglatman@outlook.com>
2022-07-03 11:14:45 -0700

    update code base

--- resources/views/rom-file/show.blade.php
+++ resources/views/rom-file/show.blade.php
@@ -7,7 +7,7 @@
             <x-list-item>BSON Object ID: {{$romFile->_id}}</x-list-item>
             <x-list-item>Filename: {{$romFile->filename}}</x-list-item>
             <x-list-item>Filesize: {{$romFile->length}} Bytes</x-list-item>
-            <x-list-item>Chunk Size: {{$romFile->chunkSize}}</x-list-item>
+            <x-list-item>Chunk Size: {{$romFile->chunkSize * 8}} Bits</x-list-item>
             <x-list-item>MD5 Hash: {{$romFile->md5}}</x-list-item>
             <x-list-item>
                 @php
