80780d4bd51a6e1a1e134067a1f4cf57a44ea568
Broccolini <bglatman@callutheran.edu>
2022-07-03 23:11:21 -0700

    update code base

--- resources/views/rom-file/index.blade.php
+++ resources/views/rom-file/index.blade.php
@@ -18,7 +18,7 @@
             @foreach($romFiles as $romFile)
                 <div
                     data-rom-file-id="{{$romFile->_id}}"
-                    class="border border-teal-500 bg-white shadow-lg rounded w-full h-full inline-grid grid-cols-1 grid-rows-[auto_auto] gap-y-2 justify-self-center p-2">
+                    class="border border-gray-200 bg-white shadow-md rounded w-full h-full inline-grid grid-cols-1 grid-rows-[auto_auto] gap-y-2 justify-self-center p-2">
                     <p>{{$romFile->filename}}</p>
                     <p>{{$romFile->length}} Bytes</p>
                     <div class="justify-self-start align-self-end">
