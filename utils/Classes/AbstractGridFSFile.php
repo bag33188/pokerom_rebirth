e1bc13f60f77ab97902aebdbb09757c97c825a80
bag33188 <bglatman@outlook.com>
2022-07-04 14:59:47 -0700

    update code base

--- utils/Classes/AbstractGridFSFile.php
+++ utils/Classes/AbstractGridFSFile.php
@@ -7,16 +7,49 @@ use Jenssegers\Mongodb\Eloquent\Model as MongoDbModel;
 /**
  * Laravel IDEA Helper does not support MongoDB objects (at least not very well).
  * The purpose of this abstract class is to provider easier hinting and implementation
- * of this app's {@see \App\Models\RomFile RomFile} model.
- *
- * @see \App\Models\RomFile RomFile
+ * for GridFS-related models.
  */
 abstract class AbstractGridFSFile extends MongoDbModel
 {
+    /**
+     * BSON Type: ObjectID (`objectId`)
+     *
+     * @var string
+     */
     public readonly string $_id;
+
+    /**
+     * BSON Type: int32 (`int`)
+     *
+     * @var int
+     */
     public readonly int $chunkSize;
+
+    /**
+     * BSON Data Type: String (`string`)
+     *
+     * @var string
+     */
     public readonly string $filename;
+
+    /**
+     * BSON Types: int64 (`long`), int32 (`int`)
+     *
+     * @var int
+     */
     public readonly int $length;
+
+    /**
+     * BSON Type: Date (`date`)
+     *
+     * @var string
+     */
     public readonly string $uploadDate;
+
+    /**
+     * BSON Data Type: String (`string`)
+     *
+     * @var string
+     */
     public readonly string $md5;
 }
