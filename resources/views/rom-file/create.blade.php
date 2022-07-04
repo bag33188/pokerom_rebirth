a3d473fcf45ea062996d2db7780a094f8979f06a
Broccolini <bglatman@callutheran.edu>
2022-07-03 23:37:00 -0700

    update code base

--- resources/views/rom-file/create.blade.php
+++ resources/views/rom-file/create.blade.php
@@ -9,19 +9,19 @@
         @unless(sizeof($romFiles) > 0)
             <h2 class="text-center text-lg mt-7">No ROM Files found in <code>{{ROM_FILES_DIRNAME}}</code> folder</h2>
         @else
-            @php
-                function strLenDesc(string $a, string $b): int {
-                    return strlen($b) - strlen($a);
-                }
-                // sort files by string length (descending)
-                usort($romFiles, 'strLenDesc');
-            @endphp
             <x-jet-validation-errors class="mb-4" />
             <form action="{{route('rom-files.store')}}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('POST')
 
                 <div class="flex flex-col">
+                    @php
+                        function strLenDesc(string $a, string $b): int {
+                            return strlen($b) - strlen($a);
+                        }
+                        // sort files by string length (descending)
+                        usort($romFiles, 'strLenDesc');
+                    @endphp
                     <label for="romFile">Rom File</label>
                     <select name="filename" id="romFile">
                         @foreach($romFiles as $romFilename)
