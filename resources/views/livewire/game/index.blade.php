ad91425153f5ffbfd3657165ae8918d2f6ec4e26
Broccolini <bglatman@callutheran.edu>
2022-07-03 22:48:35 -0700

    update code base

--- resources/views/livewire/game/index.blade.php
+++ resources/views/livewire/game/index.blade.php
@@ -3,44 +3,48 @@
         <h2 class="text-center text-lg">Pok&eacute;mon Games Library</h2>
     </x-slot>
     <div class="container mx-auto" x-data="{ open: true }">
-        <div class="w-full flex justify-center">
-            @php
-                $gamesGridInitDisplayState = \App\Enums\DisplayStatesEnum::SHOW->value;
-            @endphp
-            <button type="button" @click="open = !open"
-                    class="bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 my-4 shadow-md rounded">
-                @include("ui.show-hide", ['text'=>'Games','initialState'=>$gamesGridInitDisplayState])
-            </button>
-        </div>
-        <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 justify-items-center my-1.5 pb-4"
-             x-show="open" x-cloak>
-            @foreach($games as $game)
-                <div class="p-6 w-full bg-white rounded-lg border border-gray-200 shadow-md">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{$game->game_name}}</h5>
-                    <div class="w-full bg-white rounded-lg my-2.5">
-                        <ul class="divide-y-2 divide-gray-100">
-                            <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
-                                {{$game->game_name . ' Version'}}
-                            </li>
-                            <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
-                                Generation {{$game->generation > 0 ? number_to_roman($game->generation) : 'N/A'}}
-                            </li>
-                            <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
-                                {{$game->region}} Region
-                            </li>
-                            <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
-                                {{GameRepo::getProperGameTypeString($game->game_type)}}
-                            </li>
-                            <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
-                                Released on {{parse_date_as_readable_string($game->date_released, false)}}
-                            </li>
-                        </ul>
+        @if(count($games) < 1)
+            <h2 class="text-center text-lg mt-7">No Games Exist in database</h2>
+        @else
+            <div class="w-full flex justify-center">
+                @php
+                    $gamesGridInitDisplayState = \App\Enums\DisplayStatesEnum::SHOW->value;
+                @endphp
+                <button type="button" @click="open = !open"
+                        class="bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 my-4 shadow-md rounded">
+                    @include("ui.show-hide", ['text'=>'Games','initialState'=>$gamesGridInitDisplayState])
+                </button>
+            </div>
+            <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 justify-items-center my-1.5 pb-4"
+                 x-show="open" x-cloak>
+                @foreach($games as $game)
+                    <div class="p-6 w-full bg-white rounded-lg border border-gray-200 shadow-md">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{$game->game_name}}</h5>
+                        <div class="w-full bg-white rounded-lg my-2.5">
+                            <ul class="divide-y-2 divide-gray-100">
+                                <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
+                                    {{$game->game_name . ' Version'}}
+                                </li>
+                                <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
+                                    Generation {{$game->generation > 0 ? number_to_roman($game->generation) : 'N/A'}}
+                                </li>
+                                <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
+                                    {{$game->region}} Region
+                                </li>
+                                <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
+                                    {{GameRepo::getProperGameTypeString($game->game_type)}}
+                                </li>
+                                <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
+                                    Released on {{parse_date_as_readable_string($game->date_released, false)}}
+                                </li>
+                            </ul>
+                        </div>
+                        <button type="button"
+                                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                                wire:click="show({{$game->id}})">More Info @include('partials._more-info-icon')</button>
                     </div>
-                    <button type="button"
-                            class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                            wire:click="show({{$game->id}})">More Info @include('partials._more-info-icon')</button>
-                </div>
-            @endforeach
-        </div>
+                @endforeach
+            </div>
+        @endif
     </div>
 </div>
--- resources/views/livewire/rom/index.blade.php
+++ resources/views/livewire/rom/index.blade.php
@@ -6,61 +6,66 @@
         <h2 class="text-center text-lg">Pok&eacute;mon ROMs Library</h2>
     </x-slot>
     <div x-data="{ open: true }" id="roms-container">
-        <div class="w-full flex justify-center">
-            @php
-                $romsTableInitDisplayState = \App\Enums\DisplayStatesEnum::SHOW->value;
-            @endphp
-            <button type="button" @click="open = !open"
-                    class="bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 my-4 shadow-md rounded">
-                @include("ui.show-hide", ['text'=>'ROMs', 'initialState'=>$romsTableInitDisplayState])
-            </button>
-        </div>
-        <table class="w-full text-sm text-left text-gray-500 light:text-gray-400" x-show="open" x-cloak>
-            <thead class="bg-gray-50">
+        @if(count($roms) < 1)
+            <h2 class="text-center text-lg mt-7">No ROMs Exist in database</h2>
+        @else
+            <div class="w-full flex justify-center">
+                @php
+                    $romsTableInitDisplayState = \App\Enums\DisplayStatesEnum::SHOW->value;
+                @endphp
+                <button type="button" @click="open = !open"
+                        class="bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 my-4 shadow-md rounded">
+                    @include("ui.show-hide", ['text'=>'ROMs', 'initialState'=>$romsTableInitDisplayState])
+                </button>
+            </div>
+            <table class="w-full text-sm text-left text-gray-500 light:text-gray-400" x-show="open" x-cloak>
+                <thead class="bg-gray-50">
                 <tr class="text-xs text-gray-700 uppercase light:bg-gray-700 light:text-gray-400">
                     @for($i = 0; $i < count($romsTableColumns); $i++)
                         <th scope="col" class="px-6 py-3">{{$romsTableColumns[$i]}}</th>
                     @endfor
                 </tr>
-            </thead>
-            <tbody class="light:bg-gray-800">
-            @foreach($roms as $rom)
-                <tr data-rom-id="{{$rom->id}}"
-                    class="border-b light:border-gray-700 odd:bg-white even:bg-gray-50 odd:light:bg-gray-800 even:light:bg-gray-700">
-                    <td class="px-6 py-4">{{$rom->rom_name}}</td>
-                    <td class="px-6 py-4">{{RomRepo::getReadableRomSize($rom->rom_size)}}</td>
-                    <td class="px-6 py-4">.{{strtolower($rom->rom_type)}}</td>
-                    <td class="px-6 py-4">{{$rom->has_game ? $rom->game->game_name : 'N/A'}}</td>
-                    <td class="px-6 py-4">
-                        @if($rom->has_file)
-                            <a class="inline-flex items-center py-2 px-4 bg-blue-500 hover:bg-blue-400 text-white font-bold
+                </thead>
+                <tbody class="light:bg-gray-800">
+                @foreach($roms as $rom)
+                    <tr data-rom-id="{{$rom->id}}"
+                        class="border-b light:border-gray-700 odd:bg-white even:bg-gray-50 odd:light:bg-gray-800 even:light:bg-gray-700">
+                        <td class="px-6 py-4">{{$rom->rom_name}}</td>
+                        <td class="px-6 py-4">{{RomRepo::getReadableRomSize($rom->rom_size)}}</td>
+                        <td class="px-6 py-4">.{{strtolower($rom->rom_type)}}</td>
+                        <td class="px-6 py-4">{{$rom->has_game ? $rom->game->game_name : 'N/A'}}</td>
+                        <td class="px-6 py-4">
+                            @if($rom->has_file)
+                                <a class="inline-flex items-center py-2 px-4 bg-blue-500 hover:bg-blue-400 text-white font-bold
                                     p-0 border-b-4 border-blue-700 hover:border-blue-500 rounded active:border-t-4
                                     active:border-b-0 active:bg-blue-400"
-                               href="{{$this->getRomDownloadUrl($rom->romFile->_id, dev: true)}}"
-                               target="_blank"
-                               title="{{$rom->getRomFileName()}}">
-                                <span class="order-0">Download!</span>
-                                @include('partials._download-icon')
-                            </a>
-                        @else
-                            <p class="font-normal text-lg">No File yet :(</p>
-                        @endif
-                    </td>
-                    <td class="px-6 py-4">
-                        <button type="button" class="inline-flex items-center py-2 px-4 bg-red-500 hover:bg-red-400 text-white font-bold
+                                   href="{{$this->getRomDownloadUrl($rom->romFile->_id, dev: true)}}"
+                                   target="_blank"
+                                   title="{{$rom->getRomFileName()}}">
+                                    <span class="order-0">Download!</span>
+                                    @include('partials._download-icon')
+                                </a>
+                            @else
+                                <p class="font-normal text-lg">No File yet :(</p>
+                            @endif
+                        </td>
+                        <td class="px-6 py-4">
+                            <button type="button" class="inline-flex items-center py-2 px-4 bg-red-500 hover:bg-red-400 text-white font-bold
                                     p-0 border-b-4 border-red-700 hover:border-red-500 rounded active:border-t-4
                                     active:border-b-0 active:bg-red-400" wire:click="show({{$rom->id}})">Get Info
-                        </button>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-            <tfoot>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+                <tfoot>
                 <tr class="text-sm text-gray-700 uppercase light:bg-gray-700 light:text-gray-400">
                     <td class="px-6 py-3">Total Count:&nbsp;<span class="font-semibold">{{count($roms)}}</span></td>
-                    <td class="px-6 py-3">Total Size:&nbsp;<span class="font-semibold">{{$roms_total_size}} Bytes</span></td>
+                    <td class="px-6 py-3">Total Size:&nbsp;<span class="font-semibold">{{$roms_total_size}} Bytes</span>
+                    </td>
                 </tr>
-            </tfoot>
-        </table>
+                </tfoot>
+            </table>
+        @endif
     </div>
 </div>
