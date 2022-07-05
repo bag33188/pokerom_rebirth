3248a0d1271b2ecd91ae3dc3e595471d746ea3c5
bag33188 <bglatman@outlook.com>
2022-07-05 14:06:57 -0700

    update code base

--- utils/functions/str_to_bool.php
+++ utils/functions/str_to_bool.php
@@ -4,11 +4,12 @@ if (!function_exists('str_to_bool')) {
     /**
      * Convert string boolean value to boolean
      *
-     * @param string|null $value
+     * @param string|null $value value to cast. can be null if referenced passed-into function is null
      * @return bool
      */
     function str_to_bool(?string $value): bool
     {
+        if (empty($value)) $value = false;
         return filter_var($value, FILTER_VALIDATE_BOOL);
     }
 }
