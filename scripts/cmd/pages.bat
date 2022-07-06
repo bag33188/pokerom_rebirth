b3fef22caef4c9455ed96b0698d759fe9d76adeb
bag33188 <bglatman@outlook.com>
2022-07-05 18:40:00 -0700

    update code base

--- scripts/cmd/pages.bat
+++ scripts/cmd/pages.bat
@@ -1,12 +1,10 @@
 @ECHO OFF
 
-:OPEN_PAGES
-    SET FIREFOX=C:\Program Files\Mozilla Firefox\firefox.exe
-    REM SET REPO_URL=https://github.com/bag33188/pokerom_rebirth
-    REM START "" /B  "%FIREFOX%" %REPO_URL%
-    START "" "%FIREFOX%" https://jetstream.laravel.com/2.x/building-your-app.html
-    START "" "%FIREFOX%" https://laravel-livewire.com/docs/2.x/making-components
-    START "" "%FIREFOX%" https://laravel.com/docs/9.x/eloquent-resources
-    START "" "%FIREFOX%" https://laravel.com/docs/9.x/eloquent-serialization
-    START "" "%FIREFOX%" https://www.sitepoint.com/laravel-livewire-getting-started/
-EXIT /B 0
+
+CD %PKR%
+
+composer install & composer update & npm install & npm update & npm run dev & git add . & git commit -m "update code base" & git push
+
+ECHO Finished!
+
+PAUSE
