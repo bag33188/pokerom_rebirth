6c871387df8972cc54472b6fec59fced541c2994
bag33188 <bglatman@outlook.com>
2022-07-05 18:42:33 -0700

    update code base

---
+++ scripts/cmd/compile.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+:COMPILE
+
+    CD %PKR%
+
+    composer install & composer update & npm install & npm update & npm run dev & git status & git add . & git commit -m "update code base" & git push
+
+    ECHO Finished!
+
+    PAUSE
+
+EXIT /B 0
