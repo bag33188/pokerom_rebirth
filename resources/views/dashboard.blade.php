42b7b50328a95114c557b093958c287183d21110
bag33188 <bglatman@outlook.com>
2022-07-07 13:19:24 -0700

    update code base

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,17 +19,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                {{--                                <x-jet-welcome/>--}}
+                {{--                                                <x-jet-welcome/>--}}
 
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                    <div>
                         <span class="block h-12 w-auto">
                             @include('partials._app-logo')
                         </span>
+                    <div class="mt-5 text-2xl">
+                        {{config('app.name')}}
                     </div>
                 </div>
-                <div class="bg-gray-200 bg-opacity-25 grid grid-rows-2">
-                    <div class="p-6 border-b border-gray-200">
+                <div class="bg-gray-200 bg-opacity-25 grid grid-rows-2 grid-cols-2">
+                    <div class="p-6 border-b border-gray-200 col-span-2 row-span-1">
                         <div class="flex items-center">
                             <div class="order-0 w-8 h-8 text-gray-400"></div>
                             <div class="ml-4 text-lg order-1 text-gray-600 leading-7 font-semibold">About</div>
@@ -44,10 +45,25 @@
                             </div>
                         </div>
                     </div>
-                    <div class="p-6">
+                    <div class="p-6 border-b border-r border-gray-200 col-start-1 col-end-1 row-start-2 row-end-2">
                         <div class="flex items-center">
                             <div class="order-0 w-8 h-8 text-gray-400"></div>
                             <div class="ml-4 text-lg order-1 text-gray-600 leading-7 font-semibold">Roms</div>
+                        </div>
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-500">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit. A accusantium cupiditate delectus,
+                                dicta ducimus eveniet, fuga ipsum maxime minus nostrum nulla officiis possimus quod
+                                tempore
+                                temporibus? Facilis molestias nostrum tenetur!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-6 col-start-2 col-end-2 row-start-2 row-end-2">
+                        <div class="flex items-center">
+                            <div class="order-0 w-8 h-8 text-gray-400"></div>
+                            <div class="ml-4 text-lg order-1 text-gray-600 leading-7 font-semibold">Games</div>
                         </div>
                         <div class="ml-12">
                             <div class="mt-2 text-sm text-gray-500">
