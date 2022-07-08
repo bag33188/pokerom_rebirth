ce76743e37bc7c37700732501884c07fbfcde50d
Broccolini <bglatman@callutheran.edu>
2022-07-07 20:30:21 -0700

    fix

---
+++ utils/Classes/_Object/QueryObject.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Utils\Classes\_Object;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Database\Query\Expression;
+use JetBrains\PhpStorm\ArrayShape;
+
+class QueryObject implements Arrayable
+{
+    public Expression $query;
+    public array $bindings;
+
+    public function __construct(Expression $query, array $bindings)
+    {
+        $this->bindings = $bindings;
+        $this->query = $query;
+    }
+
+    #[ArrayShape(['query' => "\Illuminate\Database\Query\Expression", 'bindings' => "mixed"])]
+    public function toArray(): array
+    {
+        return ['query' => $this->query, 'bindings' => $this->bindings];
+    }
+
+    public function getValues(): array
+    {
+        return array_values($this->toArray());
+    }
+}
