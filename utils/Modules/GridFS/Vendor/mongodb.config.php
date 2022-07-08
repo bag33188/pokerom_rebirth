69995ab4616df9d47c4cf66322f904d5d483778c
bag33188 <bglatman@outlook.com>
2022-07-07 21:30:47 -0700

    update code base

--- utils/Modules/GridFS/Vendor/mongodb.config.php
+++ utils/Modules/GridFS/Vendor/mongodb.config.php
@@ -7,21 +7,23 @@
 namespace Utils\Modules\GridFS\Vendor;
 
 $template = <<<'php'
-'connections' => [
-    'mongodb' => [
-        'driver' => 'mongodb',
-        'host' => env('DB_HOST_SECOND', 'localhost'),
-        'port' => env('DB_PORT_SECOND', 27017),
-        'database' => env('DB_DATABASE_SECOND'),
-        'username' => env('DB_USERNAME_SECOND', 'brock'),
-        'password' => env('DB_PASSWORD_SECOND', 'secret'),
-        'options' => [
-            'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
-            'authMechanism' => env('DB_AUTHENTICATION_MECHANISM'),
-            'authSource' => env('DB_AUTHENTICATION_DATABASE'),
+return [
+    'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST_SECOND', 'localhost'),
+            'port' => env('DB_PORT_SECOND', 27017),
+            'database' => env('DB_DATABASE_SECOND'),
+            'username' => env('DB_USERNAME_SECOND', 'brock'),
+            'password' => env('DB_PASSWORD_SECOND', 'secret'),
+            'options' => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+                'authMechanism' => env('DB_AUTHENTICATION_MECHANISM'),
+                'authSource' => env('DB_AUTHENTICATION_DATABASE'),
+            ],
         ],
-    ],
-]
+    ]
+];
 php;
 
 return $template;
