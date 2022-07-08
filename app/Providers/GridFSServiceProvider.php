6a39f2445f16eea3f023854ea42ad983bd069575
Broccolini <bglatman@callutheran.edu>
2022-07-08 02:33:02 -0700

    update code base

--- app/Providers/GridFSServiceProvider.php
+++ app/Providers/GridFSServiceProvider.php
@@ -2,10 +2,6 @@
 
 namespace App\Providers;
 
-use App\Jobs\ProcessRomFileDeletion;
-use App\Jobs\ProcessRomFileDownload;
-use App\Jobs\ProcessRomFileUpload;
-use App\Services\GridFS\RomFileProcessor;
 use App\Services\GridFS\RomFilesConnection;
 use App\Services\GridFS\RomFilesDatabase;
 use Illuminate\Contracts\Foundation\Application;
@@ -24,25 +20,6 @@ class GridFSServiceProvider extends ServiceProvider implements DeferrableProvide
         //! use scoped singleton since only admin user will be invoking this logic
         $this->app->scoped(RomFilesConnection::class, function (Application $app) {
             return new RomFilesConnection($app->make(RomFilesDatabase::class));
-        });
-
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-        $this->app->bindMethod([ProcessRomFileDownload::class, 'handle'], function ($job, $app) {
-            return $job->handle($app->make(RomFileProcessor::class));
-        });
-        $this->app->bindMethod([ProcessRomFileUpload::class, 'handle'], function ($job, $app) {
-            return $job->handle($app->make(RomFileProcessor::class));
-        });
-        $this->app->bindMethod([ProcessRomFileDeletion::class, 'handle'], function ($job, $app) {
-            return $job->handle($app->make(RomFileProcessor::class));
         });
     }
 
--- app/Services/GridFS/RomFileProcessor.php
+++ app/Services/GridFS/RomFileProcessor.php
@@ -8,7 +8,7 @@ use Utils\Modules\GridFS\Support\GridFSBucketMethods;
 class RomFileProcessor extends GridFSBucketMethods
 {
     protected string $entityName = 'rom_files.processor';
-
+    
     public function __construct()
     {
         parent::__construct(GfsRomFile::getBucket());
