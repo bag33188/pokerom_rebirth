3917cc01573cc6baae46651780a587f4482889aa
bag33188 <bglatman@outlook.com>
2022-07-12 14:39:29 -0700

    update code base

--- scripts/helpers/is_dir_empty.php
+++ scripts/helpers/is_dir_empty.php
@@ -1,9 +1,10 @@
 <?php
 
-if (!function_exists('isDirEmpty')) {
-
+if (!defined('FSI_FLAGS')) {
     define('FSI_FLAGS', FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::SKIP_DOTS);
+}
 
+if (!function_exists('isDirEmpty')) {
     /**
      * Checks if a given directory is empty (devoid of files)
      *
