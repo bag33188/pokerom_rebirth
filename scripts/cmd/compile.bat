aad93507995e92fbd811ee2bff291a9e6d208888
bag33188 <bglatman@outlook.com>
2022-07-12 18:32:52 -0700

    update code base

--- scripts/cmd/compile.bat
+++ scripts/cmd/compile.bat
@@ -1,5 +1,19 @@
 @ECHO OFF
 
+:: ======================
+:: Compile Project Script
+:: ======================
+
+REM.||(
+  Windows Batchfile
+  -----------------
+  This script cannot be run on a Unix machine.
+  Batch is a scripting language invented for DOS.
+  It has no compatibility with OSX or Linux.
+  This file can only be run on a Windows machine.
+)
+
+
 :COMPILE
 
     REM assumes working directory is project directory
--- scripts/cmd/mailhog.cmd
+++ scripts/cmd/mailhog.cmd
@@ -1,5 +1,19 @@
 @ECHO OFF
 
+:: =====================
+:: Launch Mailhog Script
+:: =====================
+
+REM.||(
+  Windows Batchfile
+  -----------------
+  This script cannot be run on a Unix machine.
+  Batch is a scripting language invented for DOS.
+  It has no compatibility with OSX or Linux.
+  This file can only be run on a Windows machine.
+)
+
+
 :MAILHOG
 
     REM assumes working directory is project directory
