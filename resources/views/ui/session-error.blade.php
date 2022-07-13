6af1a7e0d726c917771df8f0cbc8a92ad72e3996
Broccolini <bglatman@callutheran.edu>
2022-07-12 19:00:07 -0700

    fix session error template

--- resources/views/ui/session-error.blade.php
+++ resources/views/ui/session-error.blade.php
@@ -1,10 +1,8 @@
 @if (session()->has('message'))
     <div class="bg-gray-100 border-t-4 border-red-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
          role="alert">
-        <div class="flex">
-            <div>
-                <p class="text-sm">{{ session('message') }}</p>
-            </div>
+        <div class="flex flex-row justify-start">
+            <p class="text-sm">{{ session('message') }}</p>
         </div>
     </div>
 @endif
