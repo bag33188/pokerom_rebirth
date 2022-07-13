c50adc9d362597d6207a3349c2e0a684f888e33b
bag33188 <bglatman@outlook.com>
2022-07-12 19:50:16 -0700

    update code base

--- resources/views/livewire/rom/show.blade.php
+++ resources/views/livewire/rom/show.blade.php
@@ -24,7 +24,8 @@
             <div class="col-start-1 col-end-1 row-start-2 row-end-2 justify-self-start h-auto flex flex-row">
                 <x-jet-button class="order-0 mr-2" type="button" wire:click="edit({{$romId}})">Edit!</x-jet-button>
                 @unless($rom->has_file || isset($rom->file_id))
-                    <x-jet-button class="order-1" wire:click="attemptToLinkRomToRomFile">Attempt ROM File Link
+                    <x-jet-button class="order-1" wire:click="attemptToLinkRomToRomFile">
+                        Attempt ROM File Link
                     </x-jet-button>
                 @else
                     <x-rom-file-download class="order-1" :rom-file="$rom->romFile">
