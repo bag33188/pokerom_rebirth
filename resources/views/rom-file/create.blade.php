09b08a9642ae95615261f8f807ff68f6bf66c016
Broccolini <bglatman@callutheran.edu>
2022-07-15 03:45:05 -0700

    update code base

--- resources/views/rom-file/create.blade.php
+++ resources/views/rom-file/create.blade.php
@@ -1,21 +1,26 @@
 @push('styles')
-    <link rel="stylesheet" <?= 'type="text/css"'; ?> href="{{ mix('css/punch.css') }}"/>
+    <link rel="stylesheet" <?= 'type="text/css"'; ?> href="{{ mix('css/punch.css') }}" />
 @endpush
+@php
+    $romFilesDirCount = sizeof($romFiles);
+@endphp
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-center text-lg">Upload a ROM File</h2>
+        <h2 class="text-center text-lg">
+            Upload a ROM File <span class="font-semibold">({{$romFilesDirCount}} found in directory)</span>
+        </h2>
     </x-slot>
     <div class="p-6">
-        @unless(sizeof($romFiles) > 0)
+        @unless($romFilesDirCount > 0)
             <h2 class="text-center text-lg mt-7">No ROM Files found in <code>{{ROM_FILES_DIRNAME}}</code> folder</h2>
         @else
-            <x-jet-validation-errors class="mb-4"/>
+            <x-jet-validation-errors class="mb-4" />
             <form action="{{route('rom-files.store')}}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('POST')
 
                 <div class="flex flex-col">
-                    <x-jet-label for="romFile" :value="__('ROM Fle')"/>
+                    <x-jet-label for="romFile" :value="__('ROM Fle')" />
                     <x-form-select name="filename" id="romFile">
                         @foreach($romFiles as $index => $romFilename)
                             <option
@@ -27,7 +32,7 @@
                 </div>
 
                 <div class="my-4">
-                    <x-punch-button type="submit" text="Upload!"/>
+                    <x-punch-button type="submit" text="Upload!" />
                 </div>
             </form>
         @endunless
