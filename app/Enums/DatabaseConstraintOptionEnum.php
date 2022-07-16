acfe15d8a34b4b7df31e2eb6193e4f081db7e870
bag33188 <bglatman@outlook.com>
2022-07-16 14:09:18 -0700

    update code base

--- app/Enums/DatabaseConstraintOptionEnum.php
+++ app/Enums/DatabaseConstraintOptionEnum.php
@@ -2,7 +2,7 @@
 
 namespace App\Enums;
 
-enum DatabaseConstraintOptionsEnum: string
+enum DatabaseConstraintOptionEnum: string
 {
     case NO_ACTION = 'NO ACTION';
     case CASCADE = 'CASCADE';
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Enums\DatabaseConstraintOptionsEnum as ConstraintOptions;
+use App\Enums\DatabaseConstraintOptionEnum as ConstraintOptions;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
