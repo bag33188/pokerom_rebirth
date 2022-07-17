6acbba77d3b33b15a7afdd7d08487bfe42a9e948
bag33188 <bglatman@outlook.com>
2022-07-17 15:02:46 -0700

    update code base

--- list_routes.php
+++ list_routes.php
@@ -5,7 +5,7 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
         <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-        <title>Miscellaneous</title>
+        <title>List Routes</title>
         <style <?= 'type="text/css"'; ?>>
             pre {
                 font-weight: bold;
@@ -14,8 +14,8 @@
     </head>
     <body>
         <?php
-            // run command, list directory contents
-            $output = `DIR /A /S /Q /N /O:N .`;
+            // run command, list routes
+            $output = `php artisan route:list`;
             echo "<pre>\n" . htmlentities($output) . "\n</pre>", "\n";
         ?>
     </body>
