0c29f7a61fb90aae533bb6be408ea045d395f917
bag33188 <bglatman@outlook.com>
2022-07-17 10:52:25 -0700

    update code base

--- misc/contents.php
+++ misc/contents.php
@@ -6,6 +6,11 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
         <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
         <title>Miscellaneous</title>
+        <style <?= 'type="text/css"'; ?>>
+            pre {
+                font-weight: bold;
+            }
+        </style>
     </head>
     <body>
         <?php
