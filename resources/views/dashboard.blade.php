a0df725ba794318c396723a5750284db20c60319
Broccolini <bglatman@callutheran.edu>
2022-07-18 03:55:21 -0700

    update code base

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,18 +37,16 @@
                 </div>
                 <div class="bg-gray-200 bg-opacity-25 grid grid-rows-3 grid-cols-2 md:grid-rows-2">
                     <div class="p-6 border-b border-gray-200 col-span-2 row-span-1">
-                        <div class="flex items-center">
-                            <div class="order-0 w-8 h-8 text-gray-400"><!-- insert icon --></div>
-                            <div class="ml-4 text-lg order-1 text-gray-600 leading-7 font-semibold">About</div>
-                        </div>
+                        <div class="ml-12 text-lg text-gray-600 leading-7 font-semibold">About</div>
                         <div class="ml-12">
                             <div class="mt-2 text-md text-gray-500">
                                 <!-- about description -->
                                 This web app is a databank of Pok&eacute;mon ROMs.
                                 It contains 40 ROMs, including all 33 core Pok&eacute;mon ROMs.
-                                <br/>
-                                <p class="italic mt-2 inline-flex flex-row">
-                                    <span>&copy; Pok&eacute;mon Company&nbsp;</span>
+                                <br />
+                                <p class="italic mt-2 inline-flex flex-row text-sm">
+                                    <span>&copy; Pok&eacute;mon Company</span>
+                                    <span>&nbsp;</span>
                                     <span id="copyright-year"><!-- js content insert --></span>
                                 </p>
                             </div>
@@ -56,29 +54,23 @@
                     </div>
                     <div
                         class="p-6 border-r border-gray-200 row-start-2 row-end-2 col-span-full md:col-start-1 md:col-end-1">
-                        <div class="flex items-center">
-                            <div class="order-0 w-8 h-8 text-gray-400"><!-- insert icon --></div>
-                            <div class="ml-4 text-lg order-1 text-gray-600 leading-7 font-semibold">Roms</div>
-                        </div>
+                        <div class="ml-12 text-lg text-gray-600 leading-7 font-semibold">Roms</div>
                         <div class="ml-12">
                             <div class="mt-2 text-sm text-gray-500">
                                 <!-- roms description -->
                                 Here you will find all your Core Pok&eacute;mon ROMs, as well as some spin-offs and
-                                ROM hacks.<br/>Feel free to download them for use with an emulator.
+                                ROM hacks.<br />Feel free to download them for use with an emulator.
                             </div>
                         </div>
                     </div>
                     <div
                         class="p-6 md:row-start-2 md:row-end-2 row-start-3 row-end-3 col-span-full md:col-start-2 md:col-end-2">
-                        <div class="flex items-center">
-                            <div class="order-0 w-8 h-8 text-gray-400"><!-- insert icon --></div>
-                            <div class="ml-4 text-lg order-1 text-gray-600 leading-7 font-semibold">Games</div>
-                        </div>
+                        <div class="ml-12 text-lg text-gray-600 leading-7 font-semibold">Games</div>
                         <div class="ml-12">
                             <div class="mt-2 text-sm text-gray-500">
                                 <!-- games description -->
                                 You can also navigate through the Games that are associated with the ROMs.
-                                <br/>
+                                <br />
                                 You will find more information about each one.
                             </div>
                         </div>
