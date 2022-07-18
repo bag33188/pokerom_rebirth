fb845b2f5c7b248ba08a7dc8ed80000adb541ef1
bag33188 <bglatman@outlook.com>
2022-07-18 12:32:51 -0700

    update code base

--- app/Http/Controllers/www/WelcomeController.php
+++ app/Http/Controllers/www/WelcomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\WWW;
 
 use App\Http\Controllers\Controller as ViewController;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 class WelcomeController extends ViewController
 {
-    public function index()
+    public function index(): Application|Factory|View
     {
         return view('welcome');
     }
