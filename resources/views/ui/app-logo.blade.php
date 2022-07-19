30e7692d4b23e7e003d817f45478e123d5770eb7
bag33188 <bglatman@outlook.com>
2022-07-19 15:09:25 -0700

    update code base

--- resources/views/ui/app-logo.blade.php
+++ resources/views/ui/app-logo.blade.php
@@ -1,5 +1,5 @@
 <img data-name="pokerom logo"
-     src="/public/assets/images/app_logo.png"
+     src="{{ asset('/images/app_logo.png') }}"
      alt="app-logo"
      class="block w-[90px] h-[32px]"
      data-width="90" data-height="32"/>
