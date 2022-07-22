b4669a5e42b7eba2203a2b79f70c0c8f89c7621b
bag33188 <bglatman@outlook.com>
2022-07-21 23:44:39 -0700

    update code base

--- utils/functions/join_html_classes.php
+++ utils/functions/join_html_classes.php
@@ -2,8 +2,10 @@
 
 if (!function_exists('joinHtmlClasses')) {
     /**
-     * @param string[] $classes
-     * @return string
+     * Convert an array of CSS classes and join them together into an HTML class-attribute string.
+     *
+     * @param string[] $classes string array of CSS classes
+     * @return string joined HTML class string
      */
     function joinHtmlClasses(array $classes): string
     {
