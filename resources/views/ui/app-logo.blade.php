83cc864e0aba8ecfa8cf9263788f2804c7985eda
bag33188 <bglatman@outlook.com>
2022-07-23 20:39:53 -0700

    fix vite issues

--- resources/views/ui/app-logo.blade.php
+++ resources/views/ui/app-logo.blade.php
@@ -1,5 +1,5 @@
 <img data-name="pokerom logo"
-     src="{{ asset('images/app_logo.png') }}"
+     src="{{ asset('assets/images/app_logo.png') }}"
      alt="pokerom-app-logo"
      class="block w-[90px] h-[32px]"
      data-width.px="90" data-height.px="32"
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 // import react from '@vitejs/plugin-react';
 // import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        laravel(["resources/css/app.css", "resources/js/app.js"]),
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: [...refreshPaths, "app/Http/Livewire/**"],
+        }),
         // react(),
         // vue({
         //     template: {
